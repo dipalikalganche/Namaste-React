3e9bf2c4c2687875597c89d3bdcc4a46099359ea
dipalikalganche <dipalikalganche@gmail.com>
2026-02-06 23:20:05 +0530

    added

--- App.js
+++ App.js
@@ -1,37 +1,85 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//react.createlement=>object =.when we rendered this to dom then it becoems html element
+/**
+ * Header
+ * -Logo
+ * -Nav Items
+ * Body
+ * -Search
+ * -ReastaurantContainer
+ *  -RestaurntCard
+ *   -Img
+ *   -Name of Res,Star Rating ,cusine ,DelievryTime
+ * Footer
+ * -Copywrite
+ * -links
+ * -Adress
+ * -Contact
+ */
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://letsfoodify.com/wp-content/uploads/2024/03/Logo-letsfoodify.png"
+        ></img>
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li> About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-//inside core react
-// const heading = React.createElement("h1", { id: "heading" }, "Namaste react");
-// console.log(heading);
+const RestaurantCard = () => {
+  return (
+    <div
+      className="RestaurantCard"
+      style={{ backgroundColor: "#e3dcdc", margin: "10px" }}
+    >
+      <img
+        className="res-logo"
+        alt="res-logo"
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,h_600/FOOD_CATALOG/IMAGES/CMS/2025/10/31/e2933f58-fbc0-46d3-b296-6ebce0516128_c3bd7775-f9c4-41c8-90aa-c1afd67d7eb6.jpeg"
+      ></img>
+      <h3>Let's Foodify</h3>
+      <h4> Spicy Paneer Burger</h4>
+      <h5> 4.5 Star</h5>
+      <h6> 38 minutes</h6>
+    </div>
+  );
+};
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="Restaurant-container">
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+      </div>
+    </div>
+  );
+};
 
-//inside jsx(transpiled before it reaches the js engine) - transpilation by parcel by the help of babel
-
-//jsx=> React.createElement => ReactElement-jS object =>
-
-//jsx is not html in javascript -it is html like syntax
-
-
-//component composition= react element+functional component
-//React Element
-const Title = () => (
-  <h1 className="head" tabIndex="5">
-    {elem}
-    Namaste React
-  </h1>
-);
-
-//React  Functional component
-const HeadingComponent2 = () => (
-  <div id="container">
-    
-    {/* <h2>{100+200}</h2> */}
-    <h1 className="heading">Namaste React functional component</h1>
-  </div>
-);
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeadingComponent2 />);
+root.render(<AppLayout />);
